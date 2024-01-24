1babf710fc3a42ad76daebf4782bef76b450bcfe
crazyz0ne <pvu42@mail.ru>
2024-01-24 14:51:09 +0300

    ZettaTest
    Complete Build

--- ZettaTest/src/components/Blockcomponent.vue
+++ ZettaTest/src/components/Blockcomponent.vue
@@ -1,79 +1,88 @@
 <template>
 
 
-    <main class="main">
+<main class="main">
+    <div class="container">
+      <div class="content">
         <aside class="aside">
-            <img src="../media/images/FAQ.png" class="aside__image" alt="FAQ">
+          <img src="../media/images/FAQ.png" class="aside__image" alt="FAQ">
         </aside>
-
         <section class="FAQ">
-            <h1 class="FAQ__section-firstchild">Frequently Asked Questions</h1>
-            <article class="FAQ__section">
-                <div class="FAQ__dropbtn" onclick="myFunction('step1', 'icon1')">
-                    <h2>Where can I watch?</h2>
-                    <img src="../media/images/xmark.png" class="FAQ__icon" alt="xmark" id="icon1">
-                </div>
-                  <div class="dropdown-content" id="step1">
-                    <p>Nibh quisque suscipit fermentum netus nulla cras porttitor euismod nulla. Orci, dictumst nec aliquet id ullamcorper venenatis. </p>
-                  </div>
-            </article>
-            <article class="FAQ__section">
-                <div class="FAQ__dropbtn" onclick="myFunction('step2', 'icon2')">
-                    <h2>Mauris id nibh eu fermentum mattis purus?</h2>
-                    <img src="../media/images/xmark.png" class="FAQ__icon" alt="xmark" id="icon2">
-                </div>
-                  <div class="dropdown-content" id="step2">
-                    <p>Nibh quisque suscipit fermentum netus nulla cras porttitor euismod nulla. Orci, dictumst nec aliquet id ullamcorper venenatis. </p>
-                  </div>
-            </article>
-            <article class="FAQ__section">
-                <div class="FAQ__dropbtn" onclick="myFunction('step3', 'icon3')">
-                    <h2>Eros imperdiet rhoncus?</h2>
-                    <img src="../media/images/xmark.png" class="FAQ__icon" alt="xmark" id="icon3">
-                </div>
-                  <div class="dropdown-content" id="step3">
-                    <p>Nibh quisque suscipit fermentum netus nulla cras porttitor euismod nulla. Orci, dictumst nec aliquet id ullamcorper venenatis. </p>
-                  </div>
-            </article>
-            <article class="FAQ__section">
-                <div class="FAQ__dropbtn" onclick="myFunction('step4', 'icon4')">
-                    <h2>Fames imperdiet quam fermentum?</h2>
-                    <img src="../media/images/xmark.png" class="FAQ__icon" alt="xmark" id="icon4">
-                </div>
-                  <div class="dropdown-content" id="step4">
-                    <p>Nibh quisque suscipit fermentum netus nulla cras porttitor euismod nulla. Orci, dictumst nec aliquet id ullamcorper venenatis. </p>
-                  </div>
-            </article>
-            <article class="FAQ__section">
-                <div class="FAQ__dropbtn" onclick="myFunction('step5','icon5')">
-                    <h2>Varius vitae, convallis amet lacus sit aliquet nibh?</h2>
-                    <img src="../media/images/xmark.png" class="FAQ__icon" alt="xmark" id="icon5">
-                </div>
-                  <div class="dropdown-content" id="step5">
-                    <p>Nibh quisque suscipit fermentum netus nulla cras porttitor euismod nulla. Orci, dictumst nec aliquet id ullamcorper venenatis. </p>
-                  </div>
-            </article>
-            <article class="FAQ__section">
-                <div class="FAQ__dropbtn" onclick="myFunction('step6', 'icon6')">
-                    <h2>Tortor nisl pellentesque sit quis orci dolor?</h2>
-                    <img src="../media/images/xmark.png" class="FAQ__icon" alt="xmark" id="icon6">
-                </div>
-                  <div class="dropdown-content" id="step6">
-                    <p>Nibh quisque suscipit fermentum netus nulla cras porttitor euismod nulla. Orci, dictumst nec aliquet id ullamcorper venenatis. </p>
-                  </div>
-            </article>
-            <article class="FAQ__section">
-                <div class="FAQ__dropbtn" onclick="myFunction('step7','icon7')">
-                    <h2>Vestibulum mauris mauris elementum proin amet auctor ipsum nibh sollicitudin?</h2>
-                    <img src="../media/images/xmark.png" class="FAQ__icon" alt="xmark"  id="icon7">
-                </div>
-                  <div class="dropdown-content" id="step7">
-                    <p>Nibh quisque suscipit fermentum netus nulla cras porttitor euismod nulla. Orci, dictumst nec aliquet id ullamcorper venenatis. </p>
-                  </div>
-            </article>
+          <h1 class="FAQ__section-firstchild">Frequently Asked Questions</h1>
+          <article class="FAQ__section">
+            <div class="FAQ__dropbtn" onclick="myFunction('step1', 'icon1')">
+              <h2 class="FAQ__title">Where can I watch?</h2>
+              <img src="../media/images/xmark.png" class="FAQ__icon" alt="xmark" id="icon1">
+            </div>
+            <div class="dropdown-content" id="step1">
+              <p>Nibh quisque suscipit fermentum netus nulla cras porttitor euismod nulla. Orci, dictumst nec aliquet id
+                ullamcorper venenatis. </p>
+            </div>
+          </article>
+          <article class="FAQ__section">
+            <div class="FAQ__dropbtn" onclick="myFunction('step2', 'icon2')">
+              <h2 class="FAQ__title">Mauris id nibh eu fermentum mattis purus?</h2>
+              <img src="../media/images/xmark.png" class="FAQ__icon" alt="xmark" id="icon2">
+            </div>
+            <div class="dropdown-content" id="step2">
+              <p>Nibh quisque suscipit fermentum netus nulla cras porttitor euismod nulla. Orci, dictumst nec aliquet id
+                ullamcorper venenatis. </p>
+            </div>
+          </article>
+          <article class="FAQ__section">
+            <div class="FAQ__dropbtn" onclick="myFunction('step3', 'icon3')">
+              <h2 class="FAQ__title">Eros imperdiet rhoncus?</h2>
+              <img src="../media/images/xmark.png" class="FAQ__icon" alt="xmark" id="icon3">
+            </div>
+            <div class="dropdown-content" id="step3">
+              <p>Nibh quisque suscipit fermentum netus nulla cras porttitor euismod nulla. Orci, dictumst nec aliquet id
+                ullamcorper venenatis. </p>
+            </div>
+          </article>
+          <article class="FAQ__section">
+            <div class="FAQ__dropbtn" onclick="myFunction('step4', 'icon4')">
+              <h2 class="FAQ__title">Fames imperdiet quam fermentum?</h2>
+              <img src="../media/images/xmark.png" class="FAQ__icon" alt="xmark" id="icon4">
+            </div>
+            <div class="dropdown-content" id="step4">
+              <p>Nibh quisque suscipit fermentum netus nulla cras porttitor euismod nulla. Orci, dictumst nec aliquet id
+                ullamcorper venenatis. </p>
+            </div>
+          </article>
+          <article class="FAQ__section">
+            <div class="FAQ__dropbtn" onclick="myFunction('step5','icon5')">
+              <h2 class="FAQ__title">Varius vitae, convallis amet lacus sit aliquet nibh?</h2>
+              <img src="../media/images/xmark.png" class="FAQ__icon" alt="xmark" id="icon5">
+            </div>
+            <div class="dropdown-content" id="step5">
+              <p>Nibh quisque suscipit fermentum netus nulla cras porttitor euismod nulla. Orci, dictumst nec aliquet id
+                ullamcorper venenatis. </p>
+            </div>
+          </article>
+          <article class="FAQ__section">
+            <div class="FAQ__dropbtn" onclick="myFunction('step6', 'icon6')">
+              <h2 class="FAQ__title">Tortor nisl pellentesque sit quis orci dolor?</h2>
+              <img src="../media/images/xmark.png" class="FAQ__icon" alt="xmark" id="icon6">
+            </div>
+            <div class="dropdown-content" id="step6">
+              <p>Nibh quisque suscipit fermentum netus nulla cras porttitor euismod nulla. Orci, dictumst nec aliquet id
+                ullamcorper venenatis. </p>
+            </div>
+          </article>
+          <article class="FAQ__section">
+            <div class="FAQ__dropbtn" onclick="myFunction('step7','icon7')">
+              <h2 class="FAQ__title">Vestibulum mauris mauris elementum proin amet auctor ipsum nibh sollicitudin?</h2>
+              <img src="../media/images/xmark.png" class="FAQ__icon" alt="xmark" id="icon7">
+            </div>
+            <div class="dropdown-content" id="step7">
+              <p>Nibh quisque suscipit fermentum netus nulla cras porttitor euismod nulla. Orci, dictumst nec aliquet id
+                ullamcorper venenatis. </p>
+            </div>
+          </article>
         </section>
-
-    </main>
+      </div>
+    </div>
+  </main>
  
 
 
